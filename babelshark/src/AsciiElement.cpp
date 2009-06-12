01cd918169d4fce19b4d2c8561d18fa205cf5d5b
thegordonemail@gmail.com <thegordonemail@gmail.com@27db9c14-54bd-11de-907f-3386865dedc4>
2009-06-12 19:01:44 +0000

    I've added inheritance and filled in some of the details, corrected some defects, and added a uint (unsigned int) class.
    git-svn-id: https://babelshark.googlecode.com/svn/branches/d_sibbernsen-InstructionImplementation@13 27db9c14-54bd-11de-907f-3386865dedc4

--- babelshark/src/AsciiElement.cpp
+++ babelshark/src/AsciiElement.cpp
@@ -1,55 +1,57 @@
 #include "..\AsciiElement.h"
 
-/*
+namespace BabelShark
+{
 //Chris's note - Dan had this out there, but I suspect the easiest element to implement will be
 //an unsigned int so it might be best to start with that one first.  After we get that working we
 //can probably just mimic the same approach in the sibling classes
 
-//Note I have **NOT** compiled this.  Just typeing and hoping I don't make mistakes at this point
-*/;
-namespace BabelShark
-{
+//If anyone can figure out why the semicolon up on line 2 is required it would be much appreciated
 
 
 	//TODO -- Implement this
 	//Will be used to read in data from packet (I think??)
-	void Interpret(char* buffer)
+	void AsciiElement::Interpret(char* buffer)
 	{
 		//Implement me!
 	}
 
 	//TODO -- Implement this
 	//will be used to Display data to the WireShark output
-	void Display()
+	char* AsciiElement::Display()
 	{
 		//Implement me!
+		return 0;
 	}
 
+
+/*  Commenting out the three functions below since we should be able to let it default to the 
+*   parent object
 	//Since this is a leaf node it won't have a child
-	Instruction* GetChild()
+	Instruction* AsciiElement::GetChild()
 	{
 		return 0;
 	}
 
 	//Since this is a leaf node it won't have a child
-	void Add(Instruction*)
+	void AsciiElement::Add(Instruction*)
 	{
 		return;
 	}
 
 	//Since this is a leaf node it won't have a child
-	void Remove(Instruction*)
+	void AsciiElement::Remove(Instruction*)
 	{
 		return;
 	}
-
+*/
 	//TODO
 	//I don't understand why this is returning a null iterator, but parent class did. :)
 	//creates a NULL iterator from the object defined in Instruction
-	void CreateIterator()
+	void AsciiElement::CreateIterator()
 	{
 		//_Iter = new NullIterator();
 		return;
 	}
-}
+	
 }
