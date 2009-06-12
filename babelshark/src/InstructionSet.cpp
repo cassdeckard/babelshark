5844284a0e82d29a7436a42103d4b3969dff4800
sibbernsen.dan <sibbernsen.dan@27db9c14-54bd-11de-907f-3386865dedc4>
2009-06-12 06:26:15 +0000

    committing UnitTest materials.  Also, applying svn:ignore to all *.ncb, *.suo, and *.user files.  This is to eliminate these useless file extensions from being included in the repo.
    git-svn-id: https://babelshark.googlecode.com/svn/branches/d_sibbernsen-InstructionImplementation@8 27db9c14-54bd-11de-907f-3386865dedc4

--- babelshark/src/InstructionSet.cpp
+++ babelshark/src/InstructionSet.cpp
@@ -1,1 +1,32 @@
 #include "..\InstructionSet.h"
+
+
+//returns the first Child of the InstructionSet list.
+Instruction* InstructionSet::GetChild()
+{
+
+}
+
+//Adds a child Instruction to the list.
+void InstructionSet::Add(Instruction*)
+{
+
+}
+
+//Removes a child Instruction from the list.
+void InstructionSet::Remove(Instruction*)
+{
+
+}
+
+//attempts to Interpret the buffer according to the children.
+void InstructionSet::Interpret(char* buffer)
+{
+
+}
+
+//Creates an iterator used to traverse the direct children.
+void InstructionSet::CreateIterator()
+{
+
+}
