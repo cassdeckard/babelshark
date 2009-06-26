a488fa431d3df341aa8053079efffc14164d6437
julie.betlach <julie.betlach@27db9c14-54bd-11de-907f-3386865dedc4>
2009-06-26 21:05:55 +0000

    git-svn-id: https://babelshark.googlecode.com/svn@130 27db9c14-54bd-11de-907f-3386865dedc4

--- trunk/wireshark-files/plugins/babelshark/cajun/include/DynamicTypeCollection.h
+++ trunk/wireshark-files/plugins/babelshark/cajun/include/DynamicTypeCollection.h
@@ -23,7 +23,13 @@ class DynamicTypeElement;
 class DynamicTypeCollection
 {
 public:
+   /** The DynamicTypeCollection function is just a simple constructor.
+     */
    DynamicTypeCollection();
+
+   /** The ~DynamicTypeCollection function is just a simple destructor.
+     * It will call the ClearCollection function.
+     */
    ~DynamicTypeCollection();
 
    typedef std::list<DynamicTypeElement> m_DynamicTypeCollection;
@@ -34,11 +40,28 @@ public:
    const_iterator Begin() const;
    const_iterator End() const;
 
+   /** The Insert function is used to insert a DynamicTypeElement in
+     * the DynamicTypeCollection.  There will be one call to the insert
+     * function for each dynamictype found in the PDI File.
+     */
    iterator Insert(const DynamicTypeElement& dynamicTypeElementToAdd);
+
    /** The ClearCollection function calls the Clear function on
      * each DynamicTypeElement in the collection.
      */
    void ClearCollection();
+
+   /** The FillDataDictionary function iterates over each DynamicTypeElement
+     * in the collection.  Each DynamicTypeElement contains a name and a
+     * list of DynamicTypeEntry objects.  For each DynamicTypeEntry, a function
+     * call will be made to add it to the DataDictionary.  The function
+     * call will be DataDictionary::AddDynamic(std::string alias, 
+     *    std::string parameter, std::string typeName)
+     * where alias is the name field from DynamicTypeElement,
+     * parameter is the first field inside DynamicTypeEntry, and
+     * typeName is the seconed field inside DynamicTypeEntry.
+     */
+   void FillDataDictionary();
 
 protected:
 
