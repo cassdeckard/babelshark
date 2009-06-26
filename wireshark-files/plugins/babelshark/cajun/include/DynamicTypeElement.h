f44ae5c460dd96b4f390b2254f43dd3748a934e5
julie.betlach <julie.betlach@27db9c14-54bd-11de-907f-3386865dedc4>
2009-06-26 19:56:24 +0000

    git-svn-id: https://babelshark.googlecode.com/svn/trunk@127 27db9c14-54bd-11de-907f-3386865dedc4

---
+++ wireshark-files/plugins/babelshark/cajun/include/DynamicTypeElement.h
@@ -0,0 +1,70 @@
+/**********************************************
+File : DynamicTypeElement.h
+***********************************************/
+
+#pragma once
+
+//#include <vector>
+#include <list>
+#include <string>
+
+
+namespace PDI
+{
+
+struct DynamicTypeEntry {
+   std::string first;
+   std::string second;
+};
+
+/** The DynamicTypeElement class holds one "dynamictype" read in from a PDI file.
+  * An example dynamictype in PDI file format follows:
+  * dynamictype BODY {
+  *    1 &ACK,
+  *    2 &INIT
+  * }
+  * where BODY is stored in the Name field in the DynamicTypeElement class
+  * and 1 &ACK is one entry in the DynamicTypeList.  Each entry is 
+  * stored in a DynamicTypeEntry object, which is just a struct with
+  * two std::string fields.  The first field is a string instead of
+  * an unsigned int, in order to make the definition more flexible.
+  *
+  * DynamicTypeMap is defined as std::list<DynamicTypeEntry>
+  * Originally we planned to use std::map<std::string, std::string>,
+  * but then realized that when we were going to access the data, 
+  * we just want to iterate over every item in the list, rather than 
+  * look-up an item by passing in the first value.
+  */
+class DynamicTypeElement
+{
+public:
+   // note: nothing virtual in here! and only one (1) pointer member
+   DynamicTypeElement();
+   DynamicTypeElement(const DynamicTypeElement& dyn_element);
+   ~DynamicTypeElement();
+
+   DynamicTypeElement& operator= (const DynamicTypeElement& dyn_element);
+
+   void SetName(const std::string& nameIn);
+   const std::string& Name() const;
+
+   typedef std::list<DynamicTypeEntry> DynamicTypeList;
+   typedef DynamicTypeList::iterator iterator;
+   typedef DynamicTypeList::const_iterator const_iterator;
+   iterator Begin();
+   iterator End();
+   const_iterator Begin() const;
+   const_iterator End() const;
+
+   iterator Insert(const DynamicTypeEntry& entry);
+   void Clear();
+
+protected:
+
+
+private:
+   std::string m_sName;
+
+};
+
+} // End namespace
