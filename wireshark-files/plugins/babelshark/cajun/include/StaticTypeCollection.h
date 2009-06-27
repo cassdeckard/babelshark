0c6cdf13ef310a40a9b8d5bb6e0ce36a35c56eb1
sibbernsen.dan <sibbernsen.dan@27db9c14-54bd-11de-907f-3386865dedc4>
2009-06-27 03:06:16 +0000

    adding in StaticType header files.
    git-svn-id: https://babelshark.googlecode.com/svn/trunk@136 27db9c14-54bd-11de-907f-3386865dedc4

---
+++ wireshark-files/plugins/babelshark/cajun/include/StaticTypeCollection.h
@@ -0,0 +1,25 @@
+#ifndef STATIC_TYPE_COLLECTION_H
+#define STATIC_TYPE_COLLECTION_H
+
+
+namespace PDI
+{
+	class StaticTypeCollection : public Singleton<StaticTypeCollection>
+	{
+		public:
+			typedef std::list<StaticTypeElement> StaticTypeDefinitionCollection;
+			typedef StaticTypeDefinitionCollection::iterator StaticTypeIterator;
+			typedef StaticTypeDefinitionCollection::const_iterator StaticTypeConstIter;
+
+			StaticTypeIterator Begin();
+			StaticTypeIterator End();
+
+			StaticTypeConstIter Begin() const;
+			StaticTypeConstIter End() const;
+
+			void Insert(StaticTypeElement* element);
+		private:
+	}
+}
+
+#endif //STATIC_TYPE_COLLECTION_H
