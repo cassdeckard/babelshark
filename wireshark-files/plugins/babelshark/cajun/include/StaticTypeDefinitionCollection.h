07b08e869ce61c29cb8afff7b012f0b2d968908f
sibbernsen.dan <sibbernsen.dan@27db9c14-54bd-11de-907f-3386865dedc4>
2009-06-27 07:33:44 +0000

    checking in comments as well as new header declarations.
    git-svn-id: https://babelshark.googlecode.com/svn/trunk@141 27db9c14-54bd-11de-907f-3386865dedc4

--- wireshark-files/plugins/babelshark/cajun/include/StaticTypeDefinitionCollection.h
+++ wireshark-files/plugins/babelshark/cajun/include/StaticTypeDefinitionCollection.h
@@ -11,13 +11,31 @@ namespace PDI
 			typedef StaticTypeDefinitionCollection::iterator StaticTypeIterator;
 			typedef StaticTypeDefinitionCollection::const_iterator StaticTypeConstIter;
 
+			/*
+			* Iterator accessors.  Both Const iterators and regular
+			*/
 			StaticTypeIterator Begin();
 			StaticTypeIterator End();
 
 			StaticTypeConstIter Begin() const;
 			StaticTypeConstIter End() const;
 
+			/**
+			Inserts a StaticTypeElement into the Collection
+			*/
 			void Insert(StaticTypeElement* element);
+			
+			/** The FillDataDictionary function iterates over each StaticTypeDefinition
+			 * in the collection.  Each DynamicTypeElement contains a name and a
+			 * list of DynamicTypeEntry objects.  For each DynamicTypeEntry, a function
+			 * call will be made to add it to the DataDictionary.  The function
+			 * call will be DataDictionary::AddDynamic(std::string alias, 
+			 *    std::string parameter, std::string typeName)
+			 * where alias is the name field from DynamicTypeElement,
+			 * parameter is the first field inside DynamicTypeEntry, and
+			 * typeName is the seconed field inside DynamicTypeEntry.
+			 */
+			void FillDataDictionary();
 		private:
 	}
 }
--- wireshark-files/plugins/babelshark/cajun/include/StaticTypeDefinitionElement.h
+++ wireshark-files/plugins/babelshark/cajun/include/StaticTypeDefinitionElement.h
@@ -1,17 +1,36 @@
-#ifndef STATIC_TYPE_DEFINITION_H
-#define STATIC_TYPE_DEFINITION_H
+#ifndef STATIC_TYPE_DECLARATION_H
+#define STATIC_TYPE_DECLARATION_H
 
 #include <string>
 #include "Elements.h"
 
 namespace PDI
 {
-	class StaticTypeDefinition : public Array
+
+/** The StaticTypeDefinition class holds one "statictype" read in from a PDI file.
+  * An example dynamictype in PDI file format follows:
+  * statictype HEADER {
+  *    "MessageID" : "UINT 8 $MSG_ID",
+  *    "Pad"       : "PAD 8",
+  *    "EventID"   : "UINT 16",
+  *    "Name"      : "ASCII 16",
+  *    "Pad2"      : "PAD 32"
+  * }
+  * where MessageID has a special mapping to our DataDictionary 
+  * through the alias $MSG_ID
+  *
+  */
+
+
+
+	class StaticTypeDeclaration : public Array
 	{
 		public:
-			StaticType(char* label, int size);
+			StaticTypeDeclaration(char* label, int size);
+			void SetLabel(char* input){_Label = input;}
 			char* GetLabel(){ return _Label;}
 			int GetSize() { return _Size;}
+			
 		private:
 			std::string _Label;
 			int _Size;
