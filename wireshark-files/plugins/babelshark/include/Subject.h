1d81e2afb283f581274e2a6dc04e6559efa4a0b0
thegordonemail@gmail.com <thegordonemail@gmail.com@27db9c14-54bd-11de-907f-3386865dedc4>
2009-07-06 03:49:00 +0000

    Added comments to observer/subject
    git-svn-id: https://babelshark.googlecode.com/svn/trunk@193 27db9c14-54bd-11de-907f-3386865dedc4

--- wireshark-files/plugins/babelshark/include/Subject.h
+++ wireshark-files/plugins/babelshark/include/Subject.h
@@ -9,20 +9,38 @@
 namespace BabelShark
 {
 
+    /** Subject is base class for the observer pattern
+      * At this point it doesn't seem like any of the child subjects would need to change this functionality
+      * as its pretty standard, but the possibility exists.
+      */
     class Subject
 	{
 		public:
+		    /** Empty constructor since no construction needed.
+		      */
             Subject() { }
+		    /** Empty destructor since no construction needed.
+		      */
             ~Subject() { }
 
+		    /** Adds the observer to the list of observers so when the notify method is called we know they want
+		      * to be notified
+		      */
             void Attach(Observer* observer);
 
+		    /** Removes the observer to the list of observers so when the notify method is called we know they
+		      * no longer care about changes to the subject.
+		      */
             void Detach(Observer* observer);
 
+		    /** Perform the highly important job of notifying the observers in the list of observers.
+		      */
             void Notify();
 
         protected:
 
+		    /** Objects who care when the subject changes.
+		      */
             std::list<Observer*> _Observers;
 
 	};
