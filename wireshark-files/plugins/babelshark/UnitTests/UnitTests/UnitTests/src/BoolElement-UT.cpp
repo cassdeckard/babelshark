e9f3f1a7f82df9b63459a8119f921f2cec478a03
thegordonemail@gmail.com <thegordonemail@gmail.com@27db9c14-54bd-11de-907f-3386865dedc4>
2009-07-06 05:08:18 +0000

    Updated unit test.
    git-svn-id: https://babelshark.googlecode.com/svn/trunk@197 27db9c14-54bd-11de-907f-3386865dedc4

--- wireshark-files/plugins/babelshark/UnitTests/UnitTests/UnitTests/src/BoolElement-UT.cpp
+++ wireshark-files/plugins/babelshark/UnitTests/UnitTests/UnitTests/src/BoolElement-UT.cpp
@@ -10,7 +10,9 @@ TEST(BoolInterpreter)
 	UT1.Interpret(buffer);
 	CHECK_EQUAL("unitTest1 : TRUE", UT1.Display());
 	char* buffer2 = new char[10];
-	buffer2 = "0000000000";
+	for(int counter = 0; counter < 10; counter++){
+		buffer2[counter] = 0;
+	}
 	UT1.Interpret(buffer2);
 	CHECK_EQUAL("unitTest1 : FALSE", UT1.Display());
 
