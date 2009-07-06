0d23e3e15dd89cfb8e0fcaae742341f05248df7d
julie.betlach <julie.betlach@27db9c14-54bd-11de-907f-3386865dedc4>
2009-07-06 02:16:33 +0000

    Comment updates only (and removing unneeded includes from BabelSharkSupport.h.)
    git-svn-id: https://babelshark.googlecode.com/svn@187 27db9c14-54bd-11de-907f-3386865dedc4

--- trunk/wireshark-files/plugins/babelshark/cajun/src/BabelSharkSupport.cpp
+++ trunk/wireshark-files/plugins/babelshark/cajun/src/BabelSharkSupport.cpp
@@ -17,56 +17,8 @@ File : BabelSharkSupport.cpp
 
 namespace PDI
 {
-
-   /*** The CreateInstructionTreeAndFillDataDictionary function is called by 
-   * the calling code (either the Main.cpp file in cajun test folder or the
-   * dissector.cpp file in babelshark include folder) after the PDI file
-   * has been read by the Read function.
-   * 
-   * The purpose of this function is to simplify the calling code, so
-   * it doesn't need to know the details about creating the instruction
-   * tree or about about filling the data dictionary.  For example, it
-   * is important that the Data Dictionary is filled before the 
-   * Instruction Tree is created.  This function will ensure that happens.
-   * 
-   * First it will call the StaticTypeCollection::FillDataDictionary 
-   * function to fill the Data Dictionary with all the statictypes that were
-   * in the PDI File.  
-   * Then it will call the DynamicTypeCollection::FillDataDictionary function
-   * to fill the data dictionary with all of the dynamictypes that were 
-   * in the PDI File. 
-   * Then it will instantiate the a TreeVisitor to create the Instruction Tree.
-   * Finally, it will call singleton destructors to delete the StaticTypeCollection
-   * singleton and the DynamicTypeCollection singleton.
-   * 
-   * The two paramters passed into this function are needed by the TreeVisitor
-   * constructor and accept function.
-   ***/
    BabelShark::Instruction* CreateInstructionTreeAndFillDataDictionary(const Element& elemRoot, bool bDisplayOutputToScreen)
    {
-      //Create a StaticTypeDeclarationElement and add it to the collection for testing purposes.
-      // TODO: Later, when we have the parser updated to parse the file, remove this.
-        //Instruction* _TestInstruction;
-        //_TestInstruction = new InstructionSet(1, "TestProtocol");
-      /*  BabelShark::InstructionSet* tempTree;
-
-        // make a static type
-        tempTree = new BabelShark::InstructionSet(1, "Header");
-        tempTree->Add(new BabelShark::UintElement(32, "Message_ID", "$MSG_ID"));
-        BabelShark::DataDictionary::Instance()->AddStatic("&HEADER", tempTree);
-
-        // make a static type
-        tempTree = new BabelShark::InstructionSet(1, "TestAck");
-        tempTree->Add(new BabelShark::UintElement(32, "Status"));
-        BabelShark::DataDictionary::Instance()->AddStatic("&ACK", tempTree);
-
-        // make another static type
-        tempTree = new BabelShark::InstructionSet(1, "TestInit");
-        tempTree->Add(new BabelShark::UintElement(7, "Age"));
-        tempTree->Add(new BabelShark::PadElement(56, "Pad"));
-        BabelShark::DataDictionary::Instance()->AddStatic("&INIT", tempTree);
-      */
-
       StaticTypeDeclarationCollection::Instance().FillDataDictionary();
       DynamicTypeCollection::Instance().FillDataDictionary();
 
@@ -78,10 +30,10 @@ namespace PDI
       StaticTypeDeclarationCollection::Release();
       DynamicTypeCollection::Release();
 
-      //TODO (JULIE) Delete this line later.  This is here so I can see tree that was created.
+      //This is here so we can use the debugger to view the instruction tree that was created.
       BabelShark::Instruction* pRootInstruction = TreeVisitor.GetInstruction();
 
-	  return pRootInstruction;
+	   return pRootInstruction;
 
    }
 
