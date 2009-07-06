790ac04a46d67fa843e844e12e2bd4ba58c55f9d
thegordonemail@gmail.com <thegordonemail@gmail.com@27db9c14-54bd-11de-907f-3386865dedc4>
2009-07-06 03:49:00 +0000

    Added comments to observer/subject
    git-svn-id: https://babelshark.googlecode.com/svn@193 27db9c14-54bd-11de-907f-3386865dedc4

--- trunk/wireshark-files/plugins/babelshark/include/Observer.h
+++ trunk/wireshark-files/plugins/babelshark/include/Observer.h
@@ -7,16 +7,26 @@ namespace BabelShark
 {
     class Subject; // forward declaration
 
-    /** TypeDefinition is the abstract base class for user-defined types.
+    /** Observer is another abstract class for the observer pattern
+      * It represents the base class for any number of concrete observers
+      * Currently these are planned to be instruction sizes or aliased instructions
+      * In the future could be more uses
       */
 	class Observer
 	{
         public:
 
+		/** Empty constructor since its an abstract class
+		  */
            Observer() { }
 
+		/** Empty destructor since its an abstract class
+		  */
            ~Observer() { }
 
+		/** This is where the actual work of the pattern will take place
+		  * Still an abstract class though, so it will happen at the concrete observer level.
+		  */
            virtual void Update(Subject* subject) { }
 
 		protected:
